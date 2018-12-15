fa9f6297509376d80486d18712bbf0f8a40e2129
Your Name <you@example.com>
2018-12-15 13:05:18 +0400

    class->module||appMOD

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var express = require('express');
-var path = require('path');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-// Define the port to run on
-app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Listen for requests
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
+///
+app.use(express.static("."));
+app.get('/', function (req, res) {
+   res.redirect('./public/index.html');
 });
+///
+///
+io.on('connection', function (socket) {
+  socket.on("", function (data) { });
+      /////
+  socket.emit("", data);
+      /////
+});
+////
+
+app.listen(3000, function(){
+  console.log("Server started");
+});
+
