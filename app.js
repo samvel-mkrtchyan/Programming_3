b9a45f0c8c8b2fe7b48731bfd5c31c58bc2d2a46
samvel-mkrtchyan <45490429+samvel-mkrtchyan@users.noreply.github.com>
2018-12-19 01:25:25 +0400

    aaa

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ function draw() {
       }
     }
   }
+
 }
 
 setInterval(draw, time);
--- public/script.js
+++ public/script.js
@@ -12,59 +12,11 @@ function setup() {
         console.log(matrix);
         createCanvas(matrix[0].length * side, matrix.length * side);
         background('#acacac');
-
-        
-        //HEO ANPAYMAN JNJEL
-
-        for (var y = 0; y < matrix.length; y++) {
-            for (var x = 0; x < matrix[y].length; x++) {
-    
-                if (matrix[y][x].index == 1) {
-                    fill("green");
-                    rect(x * side, y * side, side, side);
-                }
-                if (matrix[y][x].index == 2) {
-                    if (matrix[y][x].infected == true) {
-                        fill("blue");
-                    }
-                    else {
-                        fill("yellow");
-                    }
-    
-                    rect(x * side, y * side, side, side);
-                    matrix[y][x].acted = false;
-                }
-                if (matrix[y][x].index == 3) {
-                    if (matrix[y][x].infected == true) {
-                        fill("blue");
-                    }
-                    else {
-                        fill("red");
-                    }
-                    rect(x * side, y * side, side, side);
-                    matrix[y][x].acted = false;
-                }
-                if (matrix[y][x].index == 4) {
-                    fill("violet");
-                    rect(x * side, y * side, side, side);
-                    matrix[y][x].acted = false;
-                }
-                if (matrix[y][x].index == 5) {
-                    fill("black");
-                    rect(x * side, y * side, side, side);
-                    matrix[y][x].acted = false;
-                }
-            }
-        }
-
     });
+    
     noLoop();
-
-
 }
-function mousePressed() {
-    redraw();
-  }
+
 
 function draw() {
     background("#acacac");
