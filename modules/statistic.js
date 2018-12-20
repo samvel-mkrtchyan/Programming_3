a9975bb83eb22a6393a4c4a7ae6b5ae8a7632d4f
samvel-mkrtchyan <45490429+samvel-mkrtchyan@users.noreply.github.com>
2018-12-21 03:08:06 +0400

    stat

--- modules/statistic.js
+++ modules/statistic.js
@@ -1,40 +1,62 @@
-// var fs = require("fs");
+var fs = require("fs");
 
-// statistic = {
-//     "grass": grassScore = 0,
-//     "grassEater": grassEaterScore = 0,
-//     "predator": predatorScore = 0,
-//     "fly": flyScore = 0,
-//     "frog": frogScore = 0
-// };
+var statistic = {
+    "grass": grassScore = 0,
+    "grassEater": grassEaterScore = 0,
+    "predator": predatorScore = 0,
+    "fly": flyScore = 0,
+    "frog": frogScore = 0
+};
 
-// for (var y = 0; y < matrix.length; y++) {
-//     for (var x = 0; x < matrix[y].length; x++) {
+for (var y = 0; y < matrix.length; y++) {
+    for (var x = 0; x < matrix[y].length; x++) {
 
-//         if (matrix[y][x].index == 1) {
-//             grassScore++;
-//         }
-//         if (matrix[y][x].index == 2) {
-//             grassEaterScore++;
+        if (matrix[y][x].index == 1) {
+            statistic.grass++;
+        }
+        if (matrix[y][x].index == 2) {
+            statistic.grassEater++;
 
-//         }
-//         if (matrix[y][x].index == 3) {
-//             predatorScore++;
+        }
+        if (matrix[y][x].index == 3) {
+            statistic.predator++;
 
-//         }
-//         if (matrix[y][x].index == 4) {
-//             flyScore++;
+        }
+        if (matrix[y][x].index == 4) {
+            statistic.fly++;
 
-//         }
-//         if (matrix[y][x].index == 5) {
-//             frogScore++;
-//         }
-//     }
-// }
-// var myJSON = JSON.stringify(statistic);
+        }
+        if (matrix[y][x].index == 5) {
+            statistic.frog++;
+        }
+    }
+}
+io.on('connection', function (socket) {
+    socket.emit("getStat",st);
+});
 
-// function main() {
-//     fs.writeFileSync("statistic.json", myJSON);
+function max(a,b,c,d){
+    if (a>b){
+        y = a; 
+    }
+    else{
+        y = b;
+    }
+    if(c>y){
+        y = c;  
+    }
+    if(d>y){
+        y=d;
+    }
+    return y;
+}
 
-// }
-// main();
+
+
+var myJSON = JSON.stringify(statistic);
+
+function main() {
+    fs.writeFileSync("statistic", myJSON);
+
+}
+main();
--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ function setup() {
     socket.on("getNewMatrix", function (mtx) {
         matrix = mtx;
         console.log(matrix);
-        createCanvas(matrix[0].length * side + 80, matrix.length * side);
+        createCanvas(matrix[0].length * side + 1800, matrix.length * side);
         background('#acacac');
         redraw()
 
@@ -18,8 +18,9 @@ function setup() {
             matrix = mtx;
             redraw()
         });
+     
     });
-
+   
     noLoop();
 }
 
@@ -52,12 +53,12 @@ function draw() {
                     fill("red");
                 }
                 rect(x * side, y * side, side, side);
-                
+
             }
             if (matrix[y][x].index == 4) {
                 fill("violet");
                 rect(x * side, y * side, side, side);
-                
+
             }
             if (matrix[y][x].index == 5) {
                 fill("black");
@@ -65,4 +66,36 @@ function draw() {
             }
         }
     }
+    
+    function statistic(stat) {
+        line(matrix[0].length * side, 100, matrix[0].length * side + 1800, 100);
+        line(matrix[0].length * side, 150, matrix[0].length * side + 1800, 150);
+        line(matrix[0].length * side, 200, matrix[0].length * side + 1800, 200);
+        line(matrix[0].length * side, 250, matrix[0].length * side + 1800, 250);
+        line(matrix[0].length * side, 300, matrix[0].length * side + 1800, 300);
+        line(matrix[0].length * side, 350, matrix[0].length * side + 1800, 350);
+        line(matrix[0].length * side, 400, matrix[0].length * side + 1800, 400);
+
+        textSize(30);
+        fill("green")
+        text('Grass', matrix[0].length * side + 10, 130);
+        rect(matrix[0].length * side + 100, 110, 20, 20);
+        fill("yellow")
+        text('Grass Eater', matrix[0].length * side + 10, 180);
+        rect(matrix[0].length * side + 180, 160, 20, 20);
+        fill("red")
+        text('Predator', matrix[0].length * side + 10, 230);
+        rect(matrix[0].length * side + 130, 210, 20, 20);
+        fill("pink")
+        text('Fly', matrix[0].length * side + 10, 280);
+        rect(matrix[0].length * side + 60, 260, 20, 20);
+        fill("black")
+        text('Frog', matrix[0].length * side + 10, 330);
+        rect(matrix[0].length * side + 80, 310, 20, 20);
+
+        line(matrix[0].length * side + 220, 100, matrix[0].length * side + 220, 400);
+
+        text(stat.grass, matrix[0].length * side + 230, 130);
+    }
+    //
 }
